425f90f2d29a3cb9b58447ac83dfddc76e1999ac
Manaf Hamza <manafbinhamza@gmail.com>
2021-07-02 08:27:01 +0530

    =error cleared in pdfexport

--- app/Http/Controllers/pdfController.php
+++ app/Http/Controllers/pdfController.php
@@ -13,9 +13,11 @@ class pdfController extends Controller
 
     public function export($id)
     {
-        $data['employees'] = Employee::where(['id'=>$id])->get();
-        $data['experience'] =  Experience::where(['empId' => $id])->get();
-        $data['family'] =  Family::where(['empId' => $id])->get();
+        // $data['employees'] = Employee::where(['id'=>$id])->get();
+        $data['employees'] = DB::table('employees')->where(['id'=>$id])->get();
+        $data['experience'] =  DB::table('previous_experience')->where(['empId' => $id])->get();
+        // $data['experience'] =  Experience::where(['empId' => $id])->get();
+        $data['family'] =  DB::table('family_members')->where(['empId' => $id])->get();
         $pdf = PDF::loadView('admin/exportpdf',$data);
         //$pdf->setPaper('A4', 'landscape');
         return $pdf->download('employees.pdf');
